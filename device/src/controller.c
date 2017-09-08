a79814bf9b621ea0272b62ea4cf0b7db561a6e33
adi766 <adityakumar7846@gmail.com>
2017-09-08 10:35:12 +0000

    Fix bluetooth

--- device/src/controller.c
+++ device/src/controller.c
@@ -212,7 +212,10 @@ static future_t *start_up(void) {
 
     page_number++;
   }
-#if (BLE_INCLUDED == TRUE)
+
+  ble_offload_features_supported = false;
+
+/*#if (BLE_INCLUDED == TRUE)
   // read BLE offload features support from controller
   response = AWAIT_COMMAND(packet_factory->make_ble_read_offload_features_support());
   packet_parser->parse_ble_read_offload_features_response(response, &ble_offload_features_supported);
@@ -225,7 +228,7 @@ static future_t *start_up(void) {
       packet_parser->parse_generic_command_complete(response);
     }
   }
-#endif
+#endif*/
 
 #if (BLE_INCLUDED == TRUE)
   ble_supported = last_features_classic_page_index >= 1 && HCI_LE_HOST_SUPPORTED(features_classic[1].as_array);
